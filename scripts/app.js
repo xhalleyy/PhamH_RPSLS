fdd03855183013614097beb268ab68b131b93d92
xhalleyy <xhalleyy@gmail.com>
2024-01-19 00:26:17 -0800

    single vs cpu

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,9 @@
-import { gameLogic } from "./logic.js";
+import { gameLogic, player1Wins, player2Wins, winner, color } from "./logic.js";
 
 const rpsAPI = async () => {
     const promise = await fetch("https://scottsrpsls.azurewebsites.net/api/RockPaperScissors/GetRandomOption");
     return await promise.text();
 }
-
-let player1Wins = 0;
-let player2Wins = 0;
 
 // FOR PLAYER MODE: SINGLE OR MULTIPLAYER
 if (location.pathname.includes("playerpage")) {
@@ -59,6 +56,7 @@ if(location.pathname.includes("gamepage")) {
     let textColor = document.getElementById("text-color");
     let options = document.getElementById("options");
     let outcome = document.getElementById("outcome");
+    let next = document.getElementById("next");
     let whatWins = document.getElementById("whatWins");
     let rules = document.getElementById("rules");
 
@@ -73,7 +71,235 @@ if(location.pathname.includes("gamepage")) {
         // PLAYER VS CPU
         if(localStorage.getItem("playerMode") == "single player"){
             let cpuChoice = await rpsAPI();
-            console.log(cpuChoice);
+            gameLogic("Rock", cpuChoice);
+            gameTitle.classList.add("d-none");
+            options.classList.add("d-none");
+            rules.classList.add("d-none");
+            outcome.innerHTML = `You chose Rock. Computer chose ${cpuChoice}. <br> <span class=${color}>${winner}</span>`;
+            next.classList.remove("d-none");
+            // BEST OF 1
+            if(localStorage.getItem("gameMode") == "one") {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }
+            
+            if(localStorage.getItem("gameMode") == "five" && player1Wins == 3 || player2Wins == 3) {
+                next.addEventListener("click", () => {
+                    location.href='./endingpage.html';
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+
+            if(localStorage.getItem("gameMode") == "seven" && player1Wins == 4 || player2Wins == 4) {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
         }
     });
+
+    // PAPER CLICK EVENT
+    paper.addEventListener("click", async ()=> {
+        if(localStorage.getItem("playerMode") == "single player"){
+            let cpuChoice = await rpsAPI();
+            await gameLogic("Paper", cpuChoice);
+            gameTitle.classList.add("d-none");
+            options.classList.add("d-none");
+            rules.classList.add("d-none");
+            outcome.innerHTML = `You chose Paper. Computer chose ${cpuChoice}. <br> <span class=${color}>${winner}</span>`;
+            next.classList.remove("d-none");
+            // BEST OF 1
+            if(localStorage.getItem("gameMode") == "one") {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }
+            
+            if(localStorage.getItem("gameMode") == "five" && player1Wins == 3 || player2Wins == 3) {
+                next.addEventListener("click", () => {
+                    location.href='./endingpage.html';
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+
+            if(localStorage.getItem("gameMode") == "seven" && player1Wins == 4 || player2Wins == 4) {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+        }
+    });
+
+    // SCISSORS CLICK EVENT
+    scissors.addEventListener("click", async ()=> {
+        if(localStorage.getItem("playerMode") == "single player"){
+            let cpuChoice = await rpsAPI();
+            gameLogic("Scissors", cpuChoice);
+            gameTitle.classList.add("d-none");
+            options.classList.add("d-none");
+            rules.classList.add("d-none");
+            outcome.innerHTML = `You chose Scissors. Computer chose ${cpuChoice}. <br> <span class=${color}>${winner}</span>`;
+            next.classList.remove("d-none");
+            // BEST OF 1
+            if(localStorage.getItem("gameMode") == "one") {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }
+            
+            if(localStorage.getItem("gameMode") == "five" && player1Wins == 3 || player2Wins == 3) {
+                next.addEventListener("click", () => {
+                    location.href='./endingpage.html';
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+
+            if(localStorage.getItem("gameMode") == "seven" && player1Wins == 4 || player2Wins == 4) {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+        }
+    });
+
+    // LIZARD CLICK EVENT
+    lizard.addEventListener("click", async ()=> {
+        if(localStorage.getItem("playerMode") == "single player"){
+            let cpuChoice = await rpsAPI();
+            gameLogic("Lizard", cpuChoice);
+            gameTitle.classList.add("d-none");
+            options.classList.add("d-none");
+            rules.classList.add("d-none");
+            outcome.innerHTML = `You chose Lizard. Computer chose ${cpuChoice}. <br> <span class=${color}>${winner}</span>`;
+            next.classList.remove("d-none");
+            // BEST OF 1
+            if(localStorage.getItem("gameMode") == "one") {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }
+            
+            if(localStorage.getItem("gameMode") == "five" && player1Wins == 3 || player2Wins == 3) {
+                next.addEventListener("click", () => {
+                    location.href='./endingpage.html';
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+
+            if(localStorage.getItem("gameMode") == "seven" && player1Wins == 4 || player2Wins == 4) {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+        }
+    });
+
+    // SPOCK CLICK EVENT
+    spock.addEventListener("click", async ()=> {
+        if(localStorage.getItem("playerMode") == "single player"){
+            let cpuChoice = await rpsAPI();
+            gameLogic("Spock", cpuChoice);
+            gameTitle.classList.add("d-none");
+            options.classList.add("d-none");
+            rules.classList.add("d-none");
+            outcome.innerHTML = `You chose Spock. Computer chose ${cpuChoice}. <br> <span class=${color}>${winner}</span>`;
+            next.classList.remove("d-none");
+            // BEST OF 1
+            if(localStorage.getItem("gameMode") == "one") {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }
+            
+            if(localStorage.getItem("gameMode") == "five" && player1Wins == 3 || player2Wins == 3) {
+                next.addEventListener("click", () => {
+                    location.href='./endingpage.html';
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+
+            if(localStorage.getItem("gameMode") == "seven" && player1Wins == 4 || player2Wins == 4) {
+                next.addEventListener("click", () => {
+                    location.replace('./endingpage.html');
+                });
+            }else{
+                next.addEventListener("click", () => {
+                    gameTitle.classList.remove("d-none");
+                    options.classList.remove("d-none");
+                    rules.classList.remove("d-none");
+                    outcome.innerHTML = "";
+                    next.classList.add("d-none");
+                });
+            }
+        }
+    })
+
 }
--- scripts/logic.js
+++ scripts/logic.js
@@ -1,5 +1,143 @@
-const gameLogic = (answer1, answer2) => {
 
+let player1Wins = 0;
+let player2Wins = 0;
+let winner = "";
+let color = "";
+
+const gameLogic = (answer1, answer2) => {
+    switch (answer1) {
+        case "Rock":
+            switch (answer2) {
+                case "Rock":
+                    winner = "You guys Tied!";
+                    color = "blackLine";
+                    break;
+                case "Scissors":
+                    winner = "Rock crushes Scissors!";
+                    color = "redLine";
+                    player1Wins++;
+                    break;
+                case "Lizard":
+                    winner = "Rock crushes Lizard!";
+                    color = "redLine";
+                    player1Wins++;
+                    break;
+                case "Paper":
+                    winner = "Paper covers Rock!";
+                    color = "blueLine";
+                    player2Wins++;
+                    break;
+                default:
+                    winner = "Spock vaporizes Rock!";
+                    color = "blueLine";
+                    player2Wins++;
+            }
+            break;
+        case "Paper":
+            switch (answer2) {
+                case "Paper":
+                    winner = "You guys Tied!"
+                    color = "blackLine";
+                    break;
+                case "Rock":
+                    winner = "Paper covers Rock!";
+                    color = "redLine";
+                    player1Wins++;
+                case "Scissors":
+                    winner = "Scissors cut Paper!";
+                    color = "blueLine";
+                    player2Wins++;
+                    break;
+                case "Lizard":
+                    winner = "Lizard eats Paper!";
+                    color = "blueLine";
+                    player2Wins++;
+                default:
+                    winner = "Paper disproves Spock!";
+                    color = "redLine";
+                    player1Wins++;
+            }
+            break;
+        case "Scissors":
+            switch (answer2) {
+                case "Scissors":
+                    winner = "You guys tied!";
+                    color = "blackLine";
+                    break;
+                case "Rock":
+                    winner = "Rock crushes Scissors!";
+                    color = "blueLine";
+                    player2Wins++;
+                    break;
+                case "Paper":
+                    winner = "Scissors cut Paper!";
+                    color = "redLine";
+                    player1Wins++;
+                    break;
+                case "Lizard":
+                    winner = "Scissors decapitate Lizard!";
+                    color = "redLine";
+                    player1Wins++;
+                    break;
+                default:
+                    winner = "Spock smashes Scissors!";
+                    color = "blueLine";
+                    player2Wins++;
+            }
+            break;
+        case "Lizard":
+            switch (answer2) {
+                case "Lizard":
+                    winner = "You guys tied!";
+                    color = "blackLine";
+                    break;
+                case "Rock":
+                    winner = "Rock crushes Lizard!";
+                    color = "blueLine";
+                    player2Wins++;
+                    break;
+                case "Paper":
+                    winner = "Lizard eats Paper!";
+                    color = "redLine";
+                    player1Wins++;
+                    break;
+                case "Scissors":
+                    winner = "Scissors decapitate Lizard!";
+                    color = "blueLine";
+                    player2Wins++;
+                default:
+                    winner = "Lizard poisons Spock!";
+                    color = "redLine";
+                    player1Wins++;
+            }
+            break;
+        default:
+            switch (answer2) {
+                case "Rock":
+                    winner = "Spock vaporizes Rock!";
+                    color = "redLine";
+                    player1Wins++;
+                    break;
+                case "Paper":
+                    winner = "Paper disproves Spock!";
+                    color = "blueLine";
+                    player2Wins++;
+                    break;
+                case "Scissors":
+                    winner = "Spock smashes Scissors!";
+                    color = "redLine";
+                    player1Wins++;
+                    break;
+                case "Lizard":
+                    winner = "Lizard poisons Spock!";
+                    color = "blueLine";
+                    player2Wins++;
+                    break;
+                default:
+                    winner = "You guys Tied!";
+                    color = "blackLine";
+            }
+    }
 }
 
-export {gameLogic}
+export { gameLogic, player1Wins, player2Wins, winner, color }
